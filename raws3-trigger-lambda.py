9e506c0487c22c2e670b380eb3e1b2b3155a5bf5
Jason Feng <q15928@hotmail.com>
2019-06-24 12:33:40 +1000

    Created or update the crawler based on the folder name

--- raws3-trigger-lambda.py
+++ raws3-trigger-lambda.py
@@ -4,7 +4,6 @@ build data catalog with crawler
 
 Required env variables:
 DB_NAME
-CRAWLER_NAME
 """
 
 import boto3
@@ -25,9 +24,14 @@ def lambda_handler(event, context):
         "Bucket": source_bucket,
         "Key": key
     }
-    obj_path = "s3://" + source_bucket + "/" + key
+    folder_path = os.path.dirname("s3://" + source_bucket + "/" + key)
+    
+    # if a folder is created, just return
+    if key.endswith("/"):
+        return
+
     db_name = os.getenv("DB_NAME")
-    crawler_name = os.getenv("CRAWLER_NAME")
+    crawler_name = os.path.splitext(os.path.basename(key))[0]
     db_params, cl_params = {}, {}
     db_params["Name"] = db_name
     db_params["Description"] = "Testing data catalog with lambda"
@@ -35,7 +39,7 @@ def lambda_handler(event, context):
     cl_params["Role"] = "service-role/AWSGlueServiceRole-crawlerTest"
     cl_params["DatabaseName"] = db_name
     cl_params["Targets"] = {}
-    cl_params["Targets"]["S3Targets"] = [{"Path": obj_path}]
+    cl_params["Targets"]["S3Targets"] = [{"Path": folder_path}]
 
     try:
         waiter = s3.get_waiter("object_exists")
@@ -45,6 +49,7 @@ def lambda_handler(event, context):
         try:
             exist_db = glue.get_database(Name=db_name)
             print(exist_db)
+            glue.update_database(Name=db_name, DatabaseInput=db_params)
         except:
             db_resp = glue.create_database(DatabaseInput=db_params)
             print(f"Database {db_name} created, details: {db_resp}")
@@ -53,6 +58,7 @@ def lambda_handler(event, context):
         try:
             exist_cl = glue.get_crawler(Name=cl_params["Name"])
             print(exist_cl)
+            glue.update_crawler(**cl_params)
         except:
             cl_resp = glue.create_crawler(**cl_params)
             print(f"Crawler {cl_params['Name']} created, details: {cl_resp}")
